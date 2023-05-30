a525eb3fd82818d760d92cfdc1e49545fdb8e937
María <majspatricio@gmail.com>
2023-05-30 13:17:58 +0100

    Corregido PopUp con movimiento y gestión de horas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let today = new Date();
+today.setDate(today.getDate() - today.getDay());
+today = today.getFullYear() + "-" + _pad(today.getMonth() + 1) + "-" + _pad(today.getDate());
+
 let ec = new EventCalendar(document.getElementById('ec'), {
     view: 'timeGridWeek',
     height: '700px',
@@ -6,45 +10,77 @@ let ec = new EventCalendar(document.getElementById('ec'), {
         center: 'title',
         end: 'timeGridWeek,timeGridDay'
     },
-    events: [
-        {
-            title: 'Evento 1',
-            start: '2023-05-26T10:30:00',
-            end: '2023-05-26T12:00:00'
-        }
-    ],
+    
     editable: true,
-    scrollTime: '10:00:00',
+    locale: "es-ES",
+    scrollTime: '8:00:00',
     events: createEvents(),
+    eventClick: function (eventClickInfo) { },
     dateClick: function (dateClickInfo) {
-        
-        ec.addEvent({ start: dateClickInfo.dateStr.split('T')[0] + " 07:30", end: dateClickInfo.dateStr.split('T')[0] + " 17:30", color: "#FE6B64" })
+
+        /*ec.addEvent({ start: "2023-05-30T12:00:00", end: "2023-05-30T16:00:00", color: "#FE6B64" });*/
+        //Evento Popup
+        var popup = document.getElementById('calendar-popup');
+        var eventForm = document.getElementById('event-form');
+        var event = document.getElementById('event');
+        //Le doy la posición al popUp de donde se hizo click
+        var topPosition = dateClickInfo.jsEvent.clientY;
+        var leftPosition = dateClickInfo.jsEvent.clientX;
+        eventForm.setAttribute("style", "top:" + topPosition + "px; left:" + (leftPosition - 150) + "px;");
+        //Pendiente de que el popUp no se salga del contenedor
+        //Aparece el PopUp
+        popup.classList.toggle("show");
+        eventForm.classList.toggle("show");
+        //Le doy la fecha de inicio en donde se hizo click
+        var dateTimeClickStart = dateClickInfo.dateStr;
+        document.getElementById("event-start").setAttribute('value', dateTimeClickStart);
     },
     views: {
         timeGridWeek: { pointer: true },
         timeGridDay: { pointer: true },
     },
     dayMaxEvents: true,
-    nowIndicator: true,
-    selectable: true
-});
-//Función para abrir ventana emergente para añadir un evento nuevo.
-function createNewEvent() {
-    /*var dateStr = prompt('Introduce una fecha (YYYY-MM-DD)');
-    var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+    nowIndicator: true
+})
+var input = document.querySelector("#submit");
 
-    if (!isNaN(date.valueOf())) { // valid?
-        ec.addEvent(events[{ // this object will be "parsed" into an Event Object
-            title: 'Evento 1', // a property!
-            start: '2023-05-26T10:30:00', // a property!
-            end: '2023-05-26T12:00:00' // a property! ** see important note below about 'end' **
-        }])
-        /*{start: days[0] + " 00:00", end: days[0] + " 09:00", resourceId: 1, display: "background"}*/
-    //alert('Great. Now, update your database...');
-    /*} else {
-        alert('Invalid date.');
-    }*/
-}
+input.addEventListener('click', function () {
+
+    var eventStart = document.querySelector("#event-start").value;
+    var eventEnd = document.querySelector("#event-end").value;
+    if (eventEnd == ""){
+        eventEnd = moment(eventStart).add(30, 'm').format("YYYY-MM-DD[T]HH:mm"); 
+    };
+    var eventTitle = document.querySelector("#event-title").value;
+    var eventDetails = document.querySelector("#event-details").value;
+
+    const newEvent = {
+        start: eventStart,
+        end: eventEnd,
+        title: eventTitle,
+        display: eventDetails,
+        color: '#2CAFC2'
+    };
+    //aaaaaaaaaaaaaaaaaah
+    if (eventStart != "") {
+        ec.addEvent(newEvent);
+    }
+});
+/*
+function createNewEvent() {
+    var eventStart = document.querySelector("#event-start").value;
+    var eventEnd = document.querySelector("#event-end").value;
+    var eventTitle = document.querySelector("#event-title").value;
+    var eventDetails = document.querySelector("#event-details").value;
+
+    var newEvent = {
+        start: eventStart,
+        end: eventEnd,
+        title: eventTitle,
+        display: eventDetails
+    };
+
+};*/
 //Función para crear eventos al iniciar el calendario.
 function createEvents() {
     let days = [];
@@ -55,7 +91,10 @@ function createEvents() {
         days[i] = day.getFullYear() + "-" + _pad(day.getMonth() + 1) + "-" + _pad(day.getDate());
     }
     return [
-        { start: days[0] + " 00:00", end: days[0] + " 09:00", resourceId: 1, display: "background" }
+        { start: days[0] + " 00:00", end: days[0] + " 09:00", display: "background" },
+        { start: days[1] + " 12:00", end: days[1] + " 14:00", display: "background" },
+        { start: days[0] + " 10:00", end: days[0] + " 14:00", title: "Descripción", color: "#FE6B64" },
+        { start: days[1] + " 16:00", end: days[2] + " 08:00", title: "Descripción", color: "#B29DD9" },
     ];
 }
 function _pad(num) {
